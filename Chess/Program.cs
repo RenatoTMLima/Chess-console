3e7e48fe9dc9a148bb6ba2abb927072f818b881c
Renato T M Lima <renato.lima23@fatec.sp.gov.br>
2019-05-27 17:25:01 -0300

    Comecando a implementar o tabuleiro

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Chess.Tabuleiro;
 
 namespace Chess
 {
@@ -6,7 +7,9 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
         }
     }
 }
