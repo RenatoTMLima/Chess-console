af4155573f4e0f4864d79f58bff3268e2cfb968a
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2019-05-27 19:01:26 -0300

    Tratando erros com excecao personalizada

---
+++ Chess/Tabuleiro/TabuleiroExeption.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.tabuleiro
+{
+    class TabuleiroExeption : Exception
+    {
+        public TabuleiroExeption(string msg) : base(msg) { }
+    }
+}
