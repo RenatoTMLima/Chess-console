f97325eacea70be0a0a332af1d2a5fb77b70bb95
Renato T M Lima <renato.lima23@fatec.sp.gov.br>
2019-05-27 17:26:40 -0300

    Salvo o comeco da implementacao do tabuleiro

--- Chess/Tabuleiro/Cor.cs
+++ Chess/Tabuleiro/Cor.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace Chess.Tabuleiro
 {
-    class Cor
+    enum Cor
     {
+        Branca,
+        Preta
     }
 }
--- Chess/Tabuleiro/Peca.cs
+++ Chess/Tabuleiro/Peca.cs
@@ -6,5 +6,17 @@ namespace Chess.Tabuleiro
 {
     class Peca
     {
+        public Posicao Pos { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+        
+        public Peca(Posicao pos, Cor cor, Tabuleiro tab)
+        {
+            Pos = pos;
+            Cor = cor;
+            Tab = tab;
+            QtdMovimentos = 0;
+        }
     }
 }
